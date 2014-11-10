643808992f1b1f0723068ef83852e88d1a33ec6d
JosephCoppola <JosephCoppola@users.noreply.github.com>
2014-11-10 12:15:49 -0500

    Cleaning up box class code

--- 0_TestBed/BoundingBoxClass.h
+++ 0_TestBed/BoundingBoxClass.h
@@ -29,76 +29,55 @@ public:
 	matrix4 scaleMatAABB;
 
 public:
-	/* Constructor 	*/
 	BoundingBoxClass(String a_sInstanceName);
-	/* Copy Constructor */
+	
 	BoundingBoxClass(BoundingBoxClass const& other);
-	/* Copy Assignment Operator	*/
+	
 	BoundingBoxClass& operator=(BoundingBoxClass const& other);
-	/*	Destructor	*/
+
 	~BoundingBoxClass(void);
 
-	/* Gets the visibility of the Box (whether or not it is going to be drawn in render) */
 	bool GetOBBVisible(void);
-	/*Sets the visibility of the Box (whether or not it is going to be drawn in render)
-	Args:
-		bool a_bVisible -> true for draw false for not draw*/
+
 	void SetOBBVisible(bool a_bVisible);
 	
 	bool GetAABBVisible(void);
 
 	void SetAABBVisible(bool a_bVisisble);
 
-	/* Property:
-	GetVisible() or GetVisible()*/
 	__declspec(property(get = GetVisible, put = SetVisible)) bool Visible;
 
-	/* Gets the name of the model associated with this bounding box from model manager */
 	String GetInstanceName(void);
-	/* Property:
-	GetInstanceName()*/
+
 	__declspec(property(get = GetInstanceName)) String InstanceName;
 
-	/* Gets the centroid the bounding box */
 	vector3 GetCentroid(void);
-	/* Property:
-	GetCentroid()*/
+
 	__declspec(property(get = GetCentroid)) vector3 Centroid;
 
-	/* Get the radius of the bounding box */
+	
 	float GetRadius(void);
-	/* Property:
-	GetRadius()	*/
+
 	__declspec(property(get = GetRadius)) float Radius;
 
-	/* Gets the "ModelToWorld" matrix associated with the bounding box */
 	matrix4 GetModelMatrix(void);
-	/* Sets the "ModelToWorld" matrix associated with the bounding box */
+	
 	void SetModelMatrix(matrix4 a_ModelMatrix);
-	/* "ModelToWorld"
-	GetModelMatrix() or SetModelMatrix() */
+
 	__declspec(property(get = GetModelMatrix, put = SetModelMatrix)) matrix4 ModelMatrix;
 
-	/* Gets the color vector of this bounding box (the default color in which is going to be rendered) */
 	vector3 GetColor(void);
-	/* Sets the color vector of this bounding box (the default color in which is going to be rendered) */
+
 	void SetColor(vector3 a_v3Color);
-	/* Property:
-	GetColor() or SetColor() */
+
 	__declspec(property(get = GetColor, put = SetColor)) vector3 Color;
 
-	/* Renders the bounding box
-		Args:
-			a_vColor -> determinate the color of the box to be rendered, if MEDEFAULT
-			it will render the shape in the constructed color (white) */
 	void Render( vector3 a_vColor = MEDEFAULT);
 
 private:
-	/* Released Memory and objects allocated. */
+	
 	void Release(void);
-	/* Calculates the box from the instance
-	Args:
-		a_sInstance: The name of the instance for which the bounding sphere is going to be calculated */
+	
 	void CalculateAABB();
 	void CalculateOBB(String a_sInstance);
 };
